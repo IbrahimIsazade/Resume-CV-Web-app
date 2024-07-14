f220521580790c349594451cdbed1aa44ef5074f
IbrahimIsazade <ibrahimisazade264@gmail.com>
2024-07-14 19:42:22 +0400

    Onion Arc created

---
+++ src/Infrastructure/Persistence/Contexts/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts
+{
+    class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in this.ChangeTracker.Entries<ICreateEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = DateTime.Now;
+                }
+                else
+                {
+                    entry.Property(m => m.CreatedAt).IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
